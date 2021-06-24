da984b0dbbdd76848e91ecdede1e5f0080292fb2
Jimmy Zelinskie <jimmy@zelinskie.com>
2021-06-24 17:22:33 -0400

    migrate to buf
    Signed-off-by: Jimmy Zelinskie <jimmy@zelinskie.com>

---
+++ lib/authzed.rb
@@ -0,0 +1,14 @@
+require 'authzed/api/v0/acl_service_pb'
+require 'authzed/api/v0/acl_service_services_pb'
+require 'authzed/api/v0/core_pb'
+require 'authzed/api/v0/developer_pb'
+require 'authzed/api/v0/developer_services_pb'
+require 'authzed/api/v0/namespace_pb'
+require 'authzed/api/v0/namespace_service_pb'
+require 'authzed/api/v0/namespace_service_services_pb'
+require 'authzed/api/v0/watch_service_pb'
+require 'authzed/api/v0/watch_service_services_pb'
+require 'authzed/api/v1alpha1/schema_pb'
+
+module Authzed
+end
