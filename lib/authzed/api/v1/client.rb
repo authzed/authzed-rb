b2d20419bf2a20caa2f19e6b29bd5d0a00904015
Sam Kim <sam@authzed.com>
2021-10-21 17:24:15 -0700

    [WIP] oneof optional bug workaround
    Signed-off-by: Sam Kim <sam@authzed.com>

--- lib/authzed/api/v1/client.rb
+++ lib/authzed/api/v1/client.rb
@@ -1,33 +1,29 @@
 require 'grpc'
 
-require_relative 'schema_services_pb'
-require_relative 'watch_service_services_pb'
-require_relative 'permission_service_services_pb'
-
 module Authzed
   module Api
     module V1
       class Client
-        attr_reader :permission_service, :schema_service, :watch_service
+        attr_reader :permissions_service, :schema_service, :watch_service
 
         def initialize(target:, credentials: nil, interceptors: [], options: {}, timeout: nil)
           creds = credentials || GRPC::Core::ChannelCredentials.new
 
-          @permission_service = PermissionService::Stub.new(
+          @permissions_service = Authzed::Api::V1::PermissionsService::Stub.new(
             target,
             creds,
             timeout: timeout,
             interceptors: interceptors,
             channel_args: options,
           )
-          @schema_service = SchemaService::Stub.new(
+          @schema_service = Authzed::Api::V1::SchemaService::Stub.new(
             target,
             creds,
             timeout: timeout,
             interceptors: interceptors,
             channel_args: options,
           )
-          @watch_service = WatchService::Stub.new(
+          @watch_service = Authzed::Api::V1::WatchService::Stub.new(
             target,
             creds,
             timeout: timeout,
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -26,14 +26,14 @@ definition user {}
 describe "Client", "#schema" do
   context "with an empty database" do
     it "writes the schema" do
-      client = Authzed::Api::V1::Client.new(
+      client = Authzed::Api::V1alpha1::Client.new(
         target: "localhost:50051",
         credentials: :this_channel_is_insecure,
         interceptors: [Authzed::GrpcUtil::BearerToken.new(token: "somerandomkeyhere")],
       )
 
       resp = client.schema_service.write_schema(
-        Authzed::Api::V1::WriteSchemaRequest.new(schema: test_schema)
+        Authzed::Api::V1alpha1::WriteSchemaRequest.new(schema: test_schema)
       )
       expect(resp.object_definitions_names.length).to eq 2
       expect(resp.object_definitions_names).to include "document"
@@ -44,23 +44,69 @@ end
 
 describe "Client", "#schema" do
   context "with an existing schema" do
-    it "reads the schema" do
-      client = Authzed::Api::V1::Client.new(
+    let(:client) do
+      Authzed::Api::V1alpha1::Client.new(
         target: "localhost:50051",
         credentials: :this_channel_is_insecure,
         interceptors: [Authzed::GrpcUtil::BearerToken.new(token: "somerandomkeyhere")],
       )
-      client.schema_service.write_schema(
-        Authzed::Api::V1::WriteSchemaRequest.new(schema: test_schema)
+    end
+    let(:v1_client) do
+      Authzed::Api::V1::Client.new(
+        target: "localhost:50051",
+        credentials: :this_channel_is_insecure,
+        interceptors: [Authzed::GrpcUtil::BearerToken.new(token: "somerandomkeyhere")],
       )
+    end
 
+    before do
+      client.schema_service.write_schema(
+        Authzed::Api::V1alpha1::WriteSchemaRequest.new(schema: test_schema)
+      )
+    end
+
+    it "reads the schema" do
       resp = client.schema_service.read_schema(
-        Authzed::Api::V1::ReadSchemaRequest.new(
+        Authzed::Api::V1alpha1::ReadSchemaRequest.new(
           object_definitions_names: ['user'],
         )
       )
       expect(resp.object_definitions.length).to eq 1
       expect(resp.object_definitions[0]).to eq "/** user represents a user that can be granted role(s) */\ndefinition user {}"
+    end
+
+    it "can write a relationship" do
+      resp = v1_client.permissions_service.read_relationships(
+        Authzed::Api::V1::ReadRelationshipsRequest.new(
+          relationship_filter: Authzed::Api::V1::RelationshipFilter.new(resource_type: 'document')
+        )
+      )
+      expect(resp.count).to eq 0
+
+      resp = v1_client.permissions_service.write_relationships(
+        Authzed::Api::V1::WriteRelationshipsRequest.new(
+          updates: [
+            Authzed::Api::V1::RelationshipUpdate.new(
+              operation: Authzed::Api::V1::RelationshipUpdate::Operation::OPERATION_CREATE,
+              relationship: Authzed::Api::V1::Relationship.new(
+                resource: Authzed::Api::V1::ObjectReference.new(object_type: 'document', object_id: '1'),
+                relation: 'writer',
+                subject: Authzed::Api::V1::SubjectReference.new(
+                  object: Authzed::Api::V1::ObjectReference.new(object_type: 'user', object_id: 'sam'),
+                )
+              )
+            )
+          ]
+        )
+      )
+      expect(resp.written_at.token).not_to eq(nil)
+
+      resp = v1_client.permissions_service.read_relationships(
+        Authzed::Api::V1::ReadRelationshipsRequest.new(
+          relationship_filter: Authzed::Api::V1::RelationshipFilter.new(resource_type: 'document')
+        )
+      )
+      expect(resp.count).to eq 1
     end
   end
 end
